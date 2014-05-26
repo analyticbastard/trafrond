d8aff8041f5a18e550e8988b290521ec543b4c97
analyticbastard <albrecht.allenstein@gmail.com>
2014-05-26 23:15:15 +0200

    Added library dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,13 @@
   :url "https://github.com/denarius-exchange/trafrond"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [liberator "0.10.0"]
+                 [compojure "1.1.3"]
+                 [com.stuartsierra/component "0.2.1"]
+                 [http-kit "2.0.0"]
+                 [com.taoensso/sente "0.14.0"]]
   :source-paths ["src/server/clj" "src/client/cljs"]
+  :test-paths ["test"]
   :main denarius.core)
