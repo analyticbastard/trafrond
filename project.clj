902905a9ca0fbbb8c2ac68eeff02bffdfdecdc47
analyticbastard <albrecht.allenstein@gmail.com>
2014-05-26 14:56:32 +0200

    First commit with the general initial structure. Removed intermediate directory.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject denarius "0.1.0-SNAPSHOT"
+  :description "Denarius Trading Frontend"
+  :url "https://github.com/denarius-exchange/trafrond"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths ["src/server/clj" "src/client/cljs"]
+  :main denarius.core)
